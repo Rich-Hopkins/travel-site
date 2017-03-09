b88cb1e22551e92d56efe2608634955d5bc7618c
Rich <hopmedic@hotmail.com>
2017-03-08 22:48:12 -0500

    Finished babel lesson

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,11 +1,19 @@
 /**
  * Created by Rich Hopkins on 3/8/2017.
  */
-var $ = require('jquery'),
-    person = require('./modules/Person.js');
+var $ = require('jquery');
+    //Person = require('./modules/Person');
+
+import Person from './modules/person';
+
+class Adult extends Person{
+  payTaxes(){
+    console.log(this.name + " now owes $500 in taxes.");
+  }
+}
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
-jane.greet();
+var jane = new Adult("Jane Smith", "green");
+jane.payTaxes();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,15 +1,15 @@
 /**
  * Created by Rich Hopkins on 3/8/2017.
  */
-class Person{
+class Person {
   constructor(fullName, favColor) {
     this.name = fullName;
     this.favoriteColor = favColor;
   }
 
-  greet() {
-    console.log("Hi there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  greet(){
+    console.log("Hey there! My name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
   }
 }
 
-module.exports = Person;
+export default Person;
