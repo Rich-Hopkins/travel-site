18fb84b9ee15e8dea9cd2690a9b676e070e5a150
Rich <hopmedic@hotmail.com>
2017-03-08 23:47:20 -0500

    Responsive work on mobile header.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,19 +1,6 @@
 /**
  * Created by Rich Hopkins on 3/8/2017.
  */
-var $ = require('jquery');
-    //Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
 
-import Person from './modules/person';
-
-class Adult extends Person{
-  payTaxes(){
-    console.log(this.name + " now owes $500 in taxes.");
-  }
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
