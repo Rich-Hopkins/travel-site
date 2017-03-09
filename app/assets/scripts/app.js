538e772364738b19983c6f9d9b3f0d0f2d43ae87
Rich <hopmedic@hotmail.com>
2017-03-09 17:16:55 -0500

    Reveal on Features section

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,5 +2,7 @@
  * Created by Rich Hopkins on 3/8/2017.
  */
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
