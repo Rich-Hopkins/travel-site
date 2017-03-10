8c0610a850537e5e5c54e984226873130ba53f93
Rich <hopmedic@hotmail.com>
2017-03-09 21:08:40 -0500

    Working on sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,8 +3,10 @@
  */
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "90%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
